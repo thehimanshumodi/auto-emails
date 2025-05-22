bec82d3abf333f418d6f520f8f07d293738532ae
Himanshu modi <modihimanshu66@gmail.com>
2025-05-22 12:08:52 +0530

    Update automail.py

--- automail.py
+++ automail.py
@@ -7,7 +7,7 @@ import smtplib
 def automatic_email():
     user = input("Enter Your Name >>: ")
     email = input("Enter Your Email >>: ")
-    message = (f"Dear {user}, Welcome to Thecleverprogrammer")
+    message = (f"Dear {user}, Welcome to my Page")
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
     s.login("Enter your mail", "Enter App password")
